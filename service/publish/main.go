34dabee505c02890711f173d1816b3e985b341cc
Lss233 <i@lss233.com>
2023-02-05 17:45:57 +0800

    Feat(publish): add s3 api

--- service/publish/main.go
+++ service/publish/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
-	"github.com/cloudwego/kitex/server"
-	consul "github.com/kitex-contrib/registry-consul"
 	"log"
 	"net"
 	"toktik/config"
 	publish "toktik/kitex_gen/douyin/publish/publishservice"
+	"toktik/service/publish/storage"
+
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	consul "github.com/kitex-contrib/registry-consul"
 )
 
 func main() {
-	var err error
+	storage.Init()
+
+	svr := publish.NewServer(new(PublishServiceImpl))
+
+	err := svr.Run()
 
 	r, err := consul.NewConsulRegister(config.EnvConfig.CONSUL_ADDR)
 	if err != nil {
