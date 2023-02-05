0ca6755d3154f55028d8ab3d42b63979b3f8419f
Lss233 <i@lss233.com>
2023-02-06 01:11:07 +0800

    Fix(publish): Wire service to consul

--- service/publish/main.go
+++ service/publish/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	consul "github.com/kitex-contrib/registry-consul"
 	"log"
 	"net"
 	"toktik/config"
@@ -13,15 +15,26 @@ func main() {
 	storage.Init()
 	var err error
 
+	r, err := consul.NewConsulRegister(config.EnvConfig.CONSUL_ADDR)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	addr, err := net.ResolveTCPAddr("tcp", config.PublishServiceAddr)
 	if err != nil {
 		panic(err)
 	}
-	svr := publish.NewServer(new(PublishServiceImpl), server.WithServiceAddr(addr))
 
-	err = svr.Run()
-
+	srv := publish.NewServer(
+		new(PublishServiceImpl),
+		server.WithServiceAddr(addr),
+		server.WithRegistry(r),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+			ServiceName: config.PublishServiceName,
+		}),
+	)
+	err = srv.Run()
 	if err != nil {
-		log.Println(err.Error())
+		log.Fatal(err)
 	}
 }
