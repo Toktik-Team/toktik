96162e8bb89ab1f223885c4b2ee24048c1cde5da
HikariLan <shaokeyibb@126.com>
2023-02-06 02:19:35 +0800

    Feat(feed): Impl ListVideos(Some actions still WIP)

--- service/feed/handler.go
+++ service/feed/handler.go
@@ -2,14 +2,81 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"time"
 	feed "toktik/kitex_gen/douyin/feed"
+	"toktik/kitex_gen/douyin/user"
+	gen "toktik/repo"
+	"toktik/service/publish/storage"
 )
 
 // FeedServiceImpl implements the last service interface defined in the IDL.
 type FeedServiceImpl struct{}
 
+const (
+	OkStatusCode = 0
+)
+
+var (
+	OkStatusMsg = "OK"
+)
+
 // ListVideos implements the FeedServiceImpl interface.
 func (s *FeedServiceImpl) ListVideos(ctx context.Context, req *feed.ListFeedRequest) (resp *feed.ListFeedResponse, err error) {
-	// TODO: Your code here...
-	return
+	publish := gen.Q.Publish
+
+	latestTime, err := strconv.ParseInt(*req.LatestTime, 10, 64)
+	if err != nil {
+		latestTime = time.Now().UnixMilli()
+	}
+
+	find, err := publish.WithContext(ctx).Where(publish.CreatedAt.Lte(time.UnixMilli(latestTime))).Order(publish.CreatedAt.Desc()).Limit(30).Offset(0).Find()
+	if err != nil {
+		return nil, err
+	}
+
+	nextTime := find[len(find)].CreatedAt.UnixMilli()
+
+	var videos []*feed.Video
+	for _, m := range find {
+
+		u := &user.User{
+			Id: uint32(m.UserId),
+			// TODO: fill other fields
+		}
+
+		playUrl, err := storage.GetLink(m.FileName)
+		if err != nil {
+			_ = fmt.Errorf("failed to fetch play url: %w", err)
+			continue
+		}
+
+		coverUrl, err := storage.GetLink(m.CoverName)
+		if err != nil {
+			_ = fmt.Errorf("failed to fetch cover url: %w", err)
+			continue
+		}
+
+		videos = append(videos, &feed.Video{
+			Id:       uint32(m.ID),
+			Author:   u,
+			PlayUrl:  playUrl,
+			CoverUrl: coverUrl,
+			// TODO: finish this
+			FavoriteCount: 0,
+			// TODO: finish this
+			CommentCount: 0,
+			// TODO: finish this
+			IsFavorite: false,
+			Title:      m.Title,
+		})
+	}
+
+	return &feed.ListFeedResponse{
+		StatusCode: OkStatusCode,
+		StatusMsg:  &OkStatusMsg,
+		NextTime:   &nextTime,
+		Videos:     videos,
+	}, nil
 }
--- service/feed/main.go
+++ service/feed/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
+	"toktik/config"
 	feed "toktik/kitex_gen/douyin/feed/feedservice"
+	"toktik/service/publish/storage"
 )
 
 func main() {
-	svr := feed.NewServer(new(FeedServiceImpl))
+	storage.Init()
 
-	err := svr.Run()
+	var err error
+
+	addr, err := net.ResolveTCPAddr("tcp", config.FeedServiceAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	svr := feed.NewServer(new(FeedServiceImpl), server.WithServiceAddr(addr))
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
