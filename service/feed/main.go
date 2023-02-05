739f0c529bd0312e37d0d64912eb1920b843da6c
HikariLan <shaokeyibb@126.com>
2023-02-06 02:34:06 +0800

    Feat(feed): use consul

--- service/feed/main.go
+++ service/feed/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	consul "github.com/kitex-contrib/registry-consul"
 	"log"
 	"net"
 	"toktik/config"
@@ -11,16 +13,28 @@ import (
 func main() {
 	var err error
 
+	r, err := consul.NewConsulRegister(config.EnvConfig.CONSUL_ADDR)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	addr, err := net.ResolveTCPAddr("tcp", config.FeedServiceAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	svr := feed.NewServer(new(FeedServiceImpl), server.WithServiceAddr(addr))
+	srv := feed.NewServer(
+		new(FeedServiceImpl),
+		server.WithServiceAddr(addr),
+		server.WithRegistry(r),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+			ServiceName: config.FeedServiceName,
+		}),
+	)
 
-	err = svr.Run()
+	err = srv.Run()
 
 	if err != nil {
-		log.Println(err.Error())
+		log.Fatal(err)
 	}
 }
