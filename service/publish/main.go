31daed3b517223a83cc54879d712990bfeb83bd1
Lss233 <i@lss233.com>
2023-02-06 16:28:23 +0800

    Revert(publish): fucked up during rebase

--- service/publish/main.go
+++ service/publish/main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"github.com/cloudwego/kitex/server"
 	"log"
 	"net"
 	"toktik/config"
 	publish "toktik/kitex_gen/douyin/publish/publishservice"
-	"toktik/service/publish/storage"
+
+	"github.com/cloudwego/kitex/server"
 
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
-	"github.com/cloudwego/kitex/server"
 	consul "github.com/kitex-contrib/registry-consul"
 )
 
 func main() {
-	storage.Init()
-	var err error
-
-	addr, err := net.ResolveTCPAddr("tcp", config.PublishServiceAddr)
-	if err != nil {
-		panic(err)
-	}
-	svr := publish.NewServer(new(PublishServiceImpl), server.WithServiceAddr(addr))
-
-	err = svr.Run()
-
 	r, err := consul.NewConsulRegister(config.EnvConfig.CONSUL_ADDR)
 	if err != nil {
 		log.Fatal(err)
