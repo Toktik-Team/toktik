dafb34607b6dbbbc9158471a8ed1da02a318d6bf
nicognaw <nicognaw@outlook.com>
2023-02-21 00:04:44 +0800

    Feat: Remaining e2e tests

--- test/e2e/interact_api_test.go
+++ test/e2e/interact_api_test.go
@@ -1,5 +1,5 @@
 // Progress:
-//   WIP
+//   All tests are synced with 2023-2-20 version of the API. Not sure if the API will change in the future.
 
 package main
 
@@ -21,6 +21,11 @@ func TestFavorite(t *testing.T) {
 
 	userId, token := getTestUserToken(testUserA, e)
 
+	// Sample:
+	//{
+	//    "status_code": 0,
+	//    "status_msg": "string"
+	//}
 	favoriteResp := e.POST("/douyin/favorite/action/").
 		WithQuery("token", token).WithQuery("video_id", videoId).WithQuery("action_type", 1).
 		WithFormField("token", token).WithFormField("video_id", videoId).WithFormField("action_type", 1).
@@ -28,7 +33,37 @@ func TestFavorite(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	favoriteResp.Value("status_code").Number().Equal(0)
+	favoriteResp.Value("status_msg").String().NotEmpty()
 
+	// Sample:
+	//{
+	//    "status_code": "string",
+	//    "status_msg": "string",
+	//    "video_list": [
+	//        {
+	//            "id": 0,
+	//            "author": {
+	//                "id": 0,
+	//                "name": "string",
+	//                "follow_count": 0,
+	//                "follower_count": 0,
+	//                "is_follow": true,
+	//                "avatar": "string",
+	//                "background_image": "string",
+	//                "signature": "string",
+	//                "total_favorited": "string",
+	//                "work_count": 0,
+	//                "favorite_count": 0
+	//            },
+	//            "play_url": "string",
+	//            "cover_url": "string",
+	//            "favorite_count": 0,
+	//            "comment_count": 0,
+	//            "is_favorite": true,
+	//            "title": "string"
+	//        }
+	//    ]
+	//}
 	favoriteListResp := e.GET("/douyin/favorite/list/").
 		WithQuery("token", token).WithQuery("user_id", userId).
 		WithFormField("token", token).WithFormField("user_id", userId).
@@ -36,12 +71,17 @@ func TestFavorite(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	favoriteListResp.Value("status_code").Number().Equal(0)
+	favoriteListResp.Value("status_msg").String().NotEmpty()
 	for _, element := range favoriteListResp.Value("video_list").Array().Iter() {
 		video := element.Object()
 		video.ContainsKey("id")
-		video.ContainsKey("author")
+		ValidateUser(video.Value("author").Object())
 		video.Value("play_url").String().NotEmpty()
 		video.Value("cover_url").String().NotEmpty()
+		video.ContainsKey("favorite_count")
+		video.ContainsKey("comment_count")
+		video.ContainsKey("is_favorite")
+		video.Value("title").String().NotEmpty()
 	}
 }
 
@@ -56,6 +96,29 @@ func TestComment(t *testing.T) {
 
 	_, token := getTestUserToken(testUserA, e)
 
+	// Sample:
+	//{
+	//    "status_code": 0,
+	//    "status_msg": "string",
+	//    "comment": {
+	//        "id": 0,
+	//        "user": {
+	//            "id": 0,
+	//            "name": "string",
+	//            "follow_count": 0,
+	//            "follower_count": 0,
+	//            "is_follow": true,
+	//            "avatar": "string",
+	//            "background_image": "string",
+	//            "signature": "string",
+	//            "total_favorited": "string",
+	//            "work_count": 0,
+	//            "favorite_count": 0
+	//        },
+	//        "content": "string",
+	//        "create_date": "string"
+	//    }
+	//}
 	addCommentResp := e.POST("/douyin/comment/action/").
 		WithQuery("token", token).WithQuery("video_id", videoId).WithQuery("action_type", 1).WithQuery("comment_text", "测试评论").
 		WithFormField("token", token).WithFormField("video_id", videoId).WithFormField("action_type", 1).WithFormField("comment_text", "测试评论").
@@ -63,9 +126,36 @@ func TestComment(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	addCommentResp.Value("status_code").Number().Equal(0)
+	addCommentResp.Value("status_msg").String().NotEmpty()
 	addCommentResp.Value("comment").Object().Value("id").Number().Gt(0)
+	ValidateUser(addCommentResp.Value("comment").Object().Value("user").Object())
 	commentId := int(addCommentResp.Value("comment").Object().Value("id").Number().Raw())
 
+	// Sample:
+	//{
+	//    "status_code": 0,
+	//    "status_msg": "string",
+	//    "comment_list": [
+	//        {
+	//            "id": 0,
+	//            "user": {
+	//                "id": 0,
+	//                "name": "string",
+	//                "follow_count": 0,
+	//                "follower_count": 0,
+	//                "is_follow": true,
+	//                "avatar": "string",
+	//                "background_image": "string",
+	//                "signature": "string",
+	//                "total_favorited": "string",
+	//                "work_count": 0,
+	//                "favorite_count": 0
+	//            },
+	//            "content": "string",
+	//            "create_date": "string"
+	//        }
+	//    ]
+	//}
 	commentListResp := e.GET("/douyin/comment/list/").
 		WithQuery("token", token).WithQuery("video_id", videoId).
 		WithFormField("token", token).WithFormField("video_id", videoId).
@@ -73,11 +163,13 @@ func TestComment(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	commentListResp.Value("status_code").Number().Equal(0)
+	commentListResp.Value("status_msg").String().NotEmpty()
+
 	containTestComment := false
 	for _, element := range commentListResp.Value("comment_list").Array().Iter() {
 		comment := element.Object()
 		comment.ContainsKey("id")
-		comment.ContainsKey("user")
+		ValidateUser(comment.Value("user").Object())
 		comment.Value("content").String().NotEmpty()
 		comment.Value("create_date").String().NotEmpty()
 		if int(comment.Value("id").Number().Raw()) == commentId {
--- test/e2e/social_api_test.go
+++ test/e2e/social_api_test.go
@@ -1,5 +1,5 @@
 // Progress:
-//   WIP
+//   All tests are synced with 2023-2-20 version of the API. Not sure if the API will change in the future.
 
 package main
 
@@ -16,6 +16,11 @@ func TestRelation(t *testing.T) {
 	userIdA, tokenA := getTestUserToken(testUserA, e)
 	userIdB, tokenB := getTestUserToken(testUserB, e)
 
+	// Sample:
+	//{
+	//    "status_code": 0,
+	//    "status_msg": "string"
+	//}
 	relationResp := e.POST("/douyin/relation/action/").
 		WithQuery("token", tokenA).WithQuery("to_user_id", userIdB).WithQuery("action_type", 1).
 		WithFormField("token", tokenA).WithFormField("to_user_id", userIdB).WithFormField("action_type", 1).
@@ -23,7 +28,28 @@ func TestRelation(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	relationResp.Value("status_code").Number().Equal(0)
+	relationResp.Value("status_msg").String().NotEmpty()
 
+	// Sample:
+	//{
+	//    "status_code": "string",
+	//    "status_msg": "string",
+	//    "user_list": [
+	//        {
+	//            "id": 0,
+	//            "name": "string",
+	//            "follow_count": 0,
+	//            "follower_count": 0,
+	//            "is_follow": true,
+	//            "avatar": "string",
+	//            "background_image": "string",
+	//            "signature": "string",
+	//            "total_favorited": "string",
+	//            "work_count": 0,
+	//            "favorite_count": 0
+	//        }
+	//    ]
+	//}
 	followListResp := e.GET("/douyin/relation/follow/list/").
 		WithQuery("token", tokenA).WithQuery("user_id", userIdA).
 		WithFormField("token", tokenA).WithFormField("user_id", userIdA).
@@ -31,11 +57,13 @@ func TestRelation(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	followListResp.Value("status_code").Number().Equal(0)
+	followListResp.Value("user_list").Array().NotEmpty()
 
 	containTestUserB := false
 	for _, element := range followListResp.Value("user_list").Array().Iter() {
 		user := element.Object()
 		user.ContainsKey("id")
+		ValidateUser(user)
 		if int(user.Value("id").Number().Raw()) == userIdB {
 			containTestUserB = true
 		}
@@ -49,11 +77,13 @@ func TestRelation(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	followerListResp.Value("status_code").Number().Equal(0)
+	followerListResp.Value("user_list").Array().NotEmpty()
 
 	containTestUserA := false
 	for _, element := range followerListResp.Value("user_list").Array().Iter() {
 		user := element.Object()
 		user.ContainsKey("id")
+		ValidateUser(user)
 		if int(user.Value("id").Number().Raw()) == userIdA {
 			containTestUserA = true
 		}
@@ -67,6 +97,11 @@ func TestChat(t *testing.T) {
 	userIdA, tokenA := getTestUserToken(testUserA, e)
 	userIdB, tokenB := getTestUserToken(testUserB, e)
 
+	// Sample:
+	//{
+	//    "status_code": 0,
+	//    "status_msg": "string"
+	//}
 	messageResp := e.POST("/douyin/message/action/").
 		WithQuery("token", tokenA).WithQuery("to_user_id", userIdB).WithQuery("action_type", 1).WithQuery("content", "Send to UserB").
 		WithFormField("token", tokenA).WithFormField("to_user_id", userIdB).WithFormField("action_type", 1).WithQuery("content", "Send to UserB").
@@ -74,7 +109,23 @@ func TestChat(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	messageResp.Value("status_code").Number().Equal(0)
+	messageResp.Value("status_msg").String().NotEmpty()
 
+	// Sample:
+	//{
+	//    "status_code": "string",
+	//    "status_msg": "string",
+	//    "message_list": [
+	//        {
+	//            "id": 0,
+	//            "to_user_id": 0,
+	//            "from_user_id": 0,
+	//            "content": "string",
+	//            "pre_msg_time": "string",
+	//            "create_time": 0
+	//        }
+	//    ]
+	//}
 	chatResp := e.GET("/douyin/message/chat/").
 		WithQuery("token", tokenA).WithQuery("to_user_id", userIdB).
 		WithFormField("token", tokenA).WithFormField("to_user_id", userIdB).
@@ -82,6 +133,8 @@ func TestChat(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	chatResp.Value("status_code").Number().Equal(0)
+	chatResp.Value("status_msg").String().NotEmpty()
+
 	chatResp.Value("message_list").Array().Length().Gt(0)
 
 	chatResp = e.GET("/douyin/message/chat/").
@@ -91,5 +144,6 @@ func TestChat(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 	chatResp.Value("status_code").Number().Equal(0)
+	chatResp.Value("status_msg").String().NotEmpty()
 	chatResp.Value("message_list").Array().Length().Gt(0)
 }
