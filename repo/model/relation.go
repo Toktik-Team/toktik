fb9d44ecf1ebc52401b26501f2a429bb75607ce5
YunShu <im@yunshu.site>
2023-02-21 14:40:12 +0800

    fix: wrong gorm tag

--- repo/model/relation.go
+++ repo/model/relation.go
@@ -3,6 +3,6 @@ package model
 // Relation 关系表 /*
 type Relation struct {
 	Model           // 基础模型
-	UserId   uint32 `gorm:"not null;` // 用户ID
-	TargetId uint32 `gorm:"not null;` // 目标ID
+	UserId   uint32 `gorm:"not null"` // 用户ID
+	TargetId uint32 `gorm:"not null"` // 目标ID
 }
