4ee8a0c4c6d0be024709a103c8b040d684ac2481
HikariLan <shaokeyibb@126.com>
2023-02-22 14:04:27 +0800

    Refactor(#36): update model

--- repo/model/video.go
+++ repo/model/video.go
@@ -3,9 +3,8 @@ package model
 // Video 视频表 /*
 type Video struct {
 	Model
-	UserId        uint32 `json:"user_id"`
-	Title         string `json:"title"`
-	FileName      string `json:"play_name"`
-	CoverName     string `json:"cover_name"`
-	FavoriteCount uint32 `json:"favorite_count" gorm:"default:0"`
+	UserId    uint32 `json:"user_id"`
+	Title     string `json:"title"`
+	FileName  string `json:"play_name"`
+	CoverName string `json:"cover_name"`
 }
