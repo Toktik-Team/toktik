5537c67324d162a02eb7074cb6f73a617f2840f6
HikariLan <shaokeyibb@126.com>
2023-02-23 00:52:35 +0800

    Feature: full version of e2e test

--- test/e2e/base_api_test.go
+++ test/e2e/base_api_test.go
@@ -180,23 +180,19 @@ func TestPublish(t *testing.T) {
 	publishResp.Value("status_code").Number().Equal(0)
 	publishResp.Value("status_msg").String().NotEmpty()
 
-	// TODO: Implement publish list, when done, uncomment the following lines and improve the test
-	//publishListResp := e.GET("/douyin/publish/list/").
-	//	WithQuery("user_id", userId).WithQuery("token", token).
-	//	Expect().
-	//	Status(http.StatusOK).
-	//	JSON().Object()
-	//publishListResp.Value("status_code").Number().Equal(0)
-	//publishListResp.Value("video_list").Array().Length().Gt(0)
-	//
-	//for _, element := range publishListResp.Value("video_list").Array().Iter() {
-	//	video := element.Object()
-	//	video.ContainsKey("id")
-	//	video.ContainsKey("author")
-	//	video.Value("play_url").String().NotEmpty()
-	//	video.Value("cover_url").String().NotEmpty()
-	//}
+	publishListResp := e.GET("/douyin/publish/list/").
+		WithQuery("user_id", userId).WithQuery("token", token).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+	publishListResp.Value("status_code").Number().Equal(0)
+	publishListResp.Value("video_list").Array().Length().Gt(0)
 
-	// When the above todo is done, remove the following line
-	_ = userId
+	for _, element := range publishListResp.Value("video_list").Array().Iter() {
+		video := element.Object()
+		video.ContainsKey("id")
+		video.ContainsKey("author")
+		video.Value("play_url").String().NotEmpty()
+		video.Value("cover_url").String().NotEmpty()
+	}
 }
