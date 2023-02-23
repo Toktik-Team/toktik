f63889ee3f178e7459875eb95dd51a4e3e49fc70
HikariLan <shaokeyibb@126.com>
2023-02-23 18:07:27 +0800

    Fix: mismatch unique entry

--- repo/model/favorite.go
+++ repo/model/favorite.go
@@ -3,6 +3,6 @@ package model
 // Favorite 点赞表 /*
 type Favorite struct {
 	Model
-	UserId  uint32 `gorm:"not null;index:user_video;unique"`
-	VideoId uint32 `gorm:"not null;index:user_video;index:video;unique"`
+	UserId  uint32 `gorm:"not null;uniqueIndex:user_video"`
+	VideoId uint32 `gorm:"not null;uniqueIndex:user_video;index:video"`
 }
--- repo/model/relation.go
+++ repo/model/relation.go
@@ -3,6 +3,6 @@ package model
 // Relation 关系表 /*
 type Relation struct {
 	Model           // 基础模型
-	UserId   uint32 `gorm:"not null;index:relation_user_target,unique"` // 用户ID
-	TargetId uint32 `gorm:"not null;index:relation_user_target,unique"` // 目标ID
+	UserId   uint32 `gorm:"not null;uniqueIndex:relation_user_target"` // 用户ID
+	TargetId uint32 `gorm:"not null;uniqueIndex:relation_user_target"` // 目标ID
 }
