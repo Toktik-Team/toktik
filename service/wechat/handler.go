4fd114e0d617c74fea5d2e59d0f4564c60b6a4dc
nicognaw <nicognaw@outlook.com>
2023-02-24 09:04:54 +0800

    Fix: Further avoid duplicated messages

--- service/wechat/handler.go
+++ service/wechat/handler.go
@@ -193,7 +193,7 @@ func (s *WechatServiceImpl) WechatChat(ctx context.Context, req *wechat.MessageC
 }
 
 func (s *WechatServiceImpl) receiveChatGPT(respMsg *wechat.Message, senderID uint32) {
-	respMsg.CreateTime = time.Now().UnixMilli() + 300
+	respMsg.CreateTime = time.Now().UnixMilli() + 3000
 	*respMsg.FromUserId = 0
 	*respMsg.ToUserId = senderID
 }
